5b00d2569345bbeb8c14db3a5262b471bbc5b808
elsirion <elsirion@protonmail.com>
2025-11-28 22:30:46 +0100

    chore: set up devenv

---
+++ flake.nix
@@ -0,0 +1,88 @@
+{
+  description = "SatsHunt dev env";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        lib = nixpkgs.lib;
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+
+        build_arch_underscores =
+          lib.strings.replaceStrings [ "-" ] [ "_" ]
+            pkgs.stdenv.buildPlatform.config;
+
+        rocksdb = pkgs.rocksdb_8_11.override { enableLiburing = false; };
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "multisend";
+          version = "0.1.0";
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            cmake
+            clang
+            llvmPackages.libclang
+          ];
+
+          buildInputs = with pkgs; [
+            rocksdb
+          ];
+
+          NIX_CFLAGS_COMPILE = "-Wno-error=stringop-overflow";
+
+          buildPhase = ''
+            export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib";
+            export "ROCKSDB_${build_arch_underscores}_STATIC=true";
+            export "ROCKSDB_${build_arch_underscores}_LIB_DIR=${rocksdb}/lib/";
+            cargo build --release --frozen
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp target/release/multisend $out/bin/
+            mkdir -p $out/share/multisend
+            cp -r migrations $out/share/multisend/
+          '';
+
+          # Skip tests during build
+          doCheck = false;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+            rust-bin.nightly.latest.rustfmt
+            pkg-config
+            cmake
+            clang
+            llvmPackages.libclang
+            llvmPackages.libcxxClang
+          ];
+
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+          RUSTFMT = "${pkgs.rust-bin.nightly.latest.rustfmt}/bin/rustfmt";
+          "ROCKSDB_${build_arch_underscores}_STATIC" = "true";
+          "ROCKSDB_${build_arch_underscores}_LIB_DIR" = "${rocksdb}/lib/";
+        };
+      });
+}
